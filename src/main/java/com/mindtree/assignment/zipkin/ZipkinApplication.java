bfebdb3c223b04b94aa05520197b41bc52eb8c4f
Joydeep Basak <j_basak1981@yahoo.co.in>
2022-11-28 01:01:29 +0530

    Zipkin Server

---
+++ src/main/java/com/mindtree/assignment/zipkin/ZipkinApplication.java
@@ -0,0 +1,18 @@
+package com.mindtree.assignment.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin2.server.internal.EnableZipkinServer;
+
+
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication.class, args);
+	}
+
+}
